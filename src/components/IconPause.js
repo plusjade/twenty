8796994a47127018ff64b5a53d063ad90075122e
Jade Dominguez <plusjade@gmail.com>
2017-07-22 19:41:53 -0700

    Allow custom style for icons

--- src/components/IconPause.js
+++ src/components/IconPause.js
@@ -1,16 +1,21 @@
 import React from 'react'
 
 const IconPause = (props) => {
+  let style = Object.assign(
+    {
+      height: 30,
+      width: 30,
+      cursor: "pointer",
+      fill: "#E0E0E0",
+      margin: "auto",
+    },
+    props.style
+  )
+
   return (
     <svg
       onClick={props.onClick}
-      style={{
-        height: 40,
-        width: 40,
-        cursor: "pointer",
-        fill: "#E0E0E0",
-        margin: "auto",
-      }}
+      style={style}
       x="0px" y="0px" viewBox="0 0 90 112.5"
     >
       <g transform="translate(0,-962.36218)">
