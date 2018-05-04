b0b9583072d5a7cd8301e904b22ab2f3dfe30f8d
Jade Dominguez <plusjade@gmail.com>
2018-05-04 00:24:04 -0700

    Block editor on the bottom right

--- src/components/BlockEditor/style.js
+++ src/components/BlockEditor/style.js
@@ -2,17 +2,17 @@ export default {
   wrap: {
     default: {
       position: "fixed",
-      top: 0,
+      bottom: 0,
       right: 0,
       zIndex: 2,
       display: "flex",
       flexDirection: "column",
       transition: "all 200ms ease-in-out",
-      transform: "translateY(-70px) scale(0)",
-      transformOrigin: "top right",
+      transform: "translateY(50px) scale(0)",
+      transformOrigin: "bottom right",
     },
     active: {
-      transform: "translateY(7px) scale(1)",
+      transform: "translateY(-50px) scale(1)",
     },
   },
 }
