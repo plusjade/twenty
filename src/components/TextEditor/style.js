e961154fac3e88b3bfd942106e0aab2b5de728be
Jade Dominguez <plusjade@gmail.com>
2018-05-20 17:01:05 -0700

    TextEditor to bottom

--- src/components/TextEditor/style.js
+++ src/components/TextEditor/style.js
@@ -1,14 +1,15 @@
 export default {
   wrap: {
     position: "absolute",
-    top: 0,
+    bottom: 0,
     left: 0,
     right: 0,
     zIndex: 9999999999,
     transition: "all 300ms ease",
     borderRadius: 15,
-    transform: "translateY(-110%)",
+    transform: "translateY(110%)",
     backgroundColor: "#FFF",
+    minHeight: '18vh',
   },
   isActive: {
     transition: "all 300ms ease",
