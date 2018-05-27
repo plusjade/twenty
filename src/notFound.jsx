731d15d2a2c473f3fda54f5785c1e8faac6d1428
Jade Dominguez <plusjade@gmail.com>
2018-05-27 15:30:12 -0700

    Use relative positioning

--- src/notFound.jsx
+++ src/notFound.jsx
@@ -13,16 +13,17 @@ const notFound = () => {
     type: "words",
     content: "Sorry that video wasn't found",
     color: "#FFF",
-    positionX: '0',
-    positionY: '-15vh',
+    align: "center",
+    xRel: '0.1',
+    yRel: '0.6',
     sceneId: sceneId,
   })
   video.addBlock({
     type: "words",
     content: `${randomEmoji(3)}`,
     color: "#FFF",
-    positionX: '0',
-    positionY: '15vh',
+    xRel: '0.4',
+    yRel: '0.5',
     sceneId: sceneId,
   })
 
