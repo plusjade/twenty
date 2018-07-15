31e87127354c84608d7db64267837ff4bf37b201
Jade Dominguez <plusjade@gmail.com>
2018-07-14 20:06:08 -0700

    Allow automatic dragging for now.

--- src/blocks/BlockWords/BlockWords.jsx
+++ src/blocks/BlockWords/BlockWords.jsx
@@ -41,9 +41,10 @@ class BlockWords extends Component {
         }
 
         if (lifecycle === 'edit') {
-          this.draggable && this.draggable.enable()
+          // this.draggable && this.draggable.enable()
         } else {
-          this.draggable && this.draggable.disable()
+          // console.log('disable drag!')
+          // this.draggable && this.draggable.disable()
         }
       }
     )
@@ -52,6 +53,11 @@ class BlockWords extends Component {
   componentDidMount() {
     this.player.on('start', this.onStart)
     this.player.on('end', this.onEnd)
+    if (this.props.canEdit) {
+      setTimeout(() => {
+        this.makeDraggable()
+      }, 1000)
+    }
   }
 
   onStart = () => {
@@ -122,7 +128,7 @@ class BlockWords extends Component {
     if (this.props.canEdit) {
       this.props.videoPlayer.stageBlock(this.props.block.get('id'))
 
-      this.makeDraggable()
+      // this.makeDraggable()
     } else {
       this.props.videoPlayer.sceneTransition()
     }
