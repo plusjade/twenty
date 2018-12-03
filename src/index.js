8a0a4d8365113d31b04df88835951e5b02e37605
Jade Dominguez <plusjade@gmail.com>
2018-12-02 19:45:23 -0800

    I think I upgraded a lot of npm packages.
    I'm trying to run latest node and yarn versions so playing will all this.

--- src/index.js
+++ src/index.js
@@ -2,10 +2,7 @@ import qs from 'qs'
 import { canEditVideo } from 'lib/actions'
 import renderHome from 'pages/renderHome'
 import renderPlayer from 'pages/renderPlayer'
-import injectTapEventPlugin from 'react-tap-event-plugin'
 import './index.css'
-
-injectTapEventPlugin()
 
 const params = qs.parse(window.location.search.substring(1))
 const videoId = params.id
